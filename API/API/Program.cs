302a5e6816720a63a7dd6a146ea1fc2b1101f47d
Auto Logon <autologon@CSED.LAB>
2024-03-22 11:36:07 -0300

    Criação de produtos

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<Produto> produtos = new List<Produto>();
+
+produtos.Add(new Produto("Celular", "IOS"));
+produtos.Add(new Produto("Celular", "Android"));
+produtos.Add(new Produto("Computador", "Windows 10"));
+
+//EndPoint - Funcionalidades
+app.MapGet("/", () => "Minha API em C#!");
+// GET Lista de produtos
+app.MapGet("api/produto/listar", () => produtos);
+// POST Cadastro de produtos
+app.MapPost("api/produto/cadastrar", () => "Cadastro de produtos!");
 
 app.Run();
+
+record Produto(string Nome, string Descricao);
